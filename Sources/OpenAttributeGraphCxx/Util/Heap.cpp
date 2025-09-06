OpenSwiftUIProject/OpenGraph

Add Heap and HashTable

Sources/OpenGraphCxx/Util/Heap.cpp
@@ -1,0 +1,99 @@
+//
+//  Heap.cpp
+//  OpenGraphCxx
+//
+//  Status: Complete
+//  Modified based Compute code
+
+#include <OpenGraphCxx/Util/Heap.hpp>
+#include <algorithm>
+
+namespace util {
+
+constexpr size_t default_increment = 0x2000;
+
+Heap *Heap::create(char *_Nullable start, size_t capacity, size_t increment) {
+    return new Heap(start, capacity, increment);
+}
+
+void Heap::destroy(Heap *value) { delete value; }
+
+Heap::Heap(char *start, size_t capacity, size_t increment) {
+    // enforce minimum but treat 0 as the default
+    size_t effective_increment = increment > 0 ? std::max(increment, minimum_increment) : default_increment;
+
+    _increment = effective_increment;
+    _node = nullptr;
+    reset(start, capacity);
+};
+
+util::Heap::~Heap() { reset(nullptr, 0); }
+
+void *util::Heap::alloc_(size_t size) {
+    if (_capacity >= size) {
+        char *result = _free_start;
+        _free_start += size;
+        _capacity -= size;
+        return result;
+    }
+
+    if (size <= minimum_increment) {
+        int64_t increment = _increment;
+        char *buffer = static_cast<char *>(malloc(increment));
+
+        _free_start = buffer;
+        _capacity = increment;
+
+        Node *node = alloc<Node>();
+
+        node->next = _node;
+        node->buffer = buffer;
+        _node = node;
+
+        char *result = _free_start;
+        _free_start += size;
+        _capacity -= size;
+        return result;
+    }
+
+    Node *node = alloc<Node>();
+    void *result = malloc(size);
+    if (result) {
+        node->next = _node;
+        node->buffer = result;
+        _node = node;
+    }
+    return result;
+}
+
+void util::Heap::reset(char *_Nullable start, size_t capacity) {
+    while (_node) {
+        void *buffer = _node->buffer;
+        _node = _node->next;
+        free(buffer);
+    }
+
+    constexpr uintptr_t alignment_mask = sizeof(char *) - 1;
+    char *aligned_start = (char *)(((uintptr_t)start + alignment_mask) & ~alignment_mask);
+
+    bool prealigned = ((uintptr_t)start & alignment_mask) == 0;
+    _free_start = prealigned ? start : aligned_start;
+    _capacity = capacity + (start - aligned_start);
+}
+
+size_t util::Heap::num_nodes() const {
+    size_t count = 0;
+    for (Node *node = _node; node != nullptr; node = node->next) {
+        count += 1;
+    }
+    return count;
+}
+
+void util::Heap::print() const {
+    fprintf(stdout, "Nodes\n");
+    for (Node *node = _node; node != nullptr; node = node->next) {
+        fprintf(stdout, "address=%-16p; buffer=%-16p; next=%-16p\n", node, node->buffer, node->next);
+    }
+}
+
+}; /* namespace util */

Sources/OpenGraphCxx/include/OpenGraphCxx/Util/ForwardList.hpp
@@ -1,0 +1,179 @@
+//
+//  ForwardList.hpp
+//  OpenGraphCxx
+//
+//  Status: Complete
+//  Modified based Compute code
+
+#ifndef OPENGRAPH_CXX_UTIL_FORWARD_LIST_HPP
+#define OPENGRAPH_CXX_UTIL_FORWARD_LIST_HPP
+
+#include <OpenGraph/OGBase.h>
+#include <OpenGraphCxx/Util/Heap.hpp>
+
+OG_ASSUME_NONNULL_BEGIN
+
+namespace util {
+
+/// ForwardList is a linked list container that uses util::Heap to allocate nodes,
+/// reusing previously removed nodes where possible.
+template <typename T> class ForwardList {
+  public:
+    using reference = T &;
+    using const_reference = const T &;
+
+  private:
+    struct Node {
+        Node *_Nullable next;
+        T value;
+    };
+
+    util::Heap *_heap;
+    Node *_Nullable _front;
+    Node *_Nullable _spare;
+    bool _is_heap_owner;
+
+  public:
+    ForwardList();
+    ForwardList(util::Heap *heap);
+    ~ForwardList();
+
+    // non-copyable
+    ForwardList(const ForwardList &) = delete;
+    ForwardList &operator=(const ForwardList &) = delete;
+
+    // non-movable
+    ForwardList(ForwardList &&) = delete;
+    ForwardList &operator=(ForwardList &&) = delete;
+
+    // MARK: Element access
+
+    reference front();
+    const_reference front() const;
+
+    // MARK: Capacity
+
+    bool empty() const noexcept { return _front == nullptr; }
+
+    // MARK: Modifiers
+
+    void push_front(const T &value);
+    void push_front(T &&value);
+    template <class... Args> void emplace_front(Args &&...args);
+
+    void pop_front();
+};
+
+template <typename T>
+ForwardList<T>::ForwardList() : _heap(new Heap(nullptr, 0, util::Heap::minimum_increment)), _is_heap_owner(true){};
+
+template <typename T> ForwardList<T>::ForwardList(util::Heap *heap) : _heap(heap), _is_heap_owner(false){};
+
+template <typename T> ForwardList<T>::~ForwardList() {
+    if (_is_heap_owner && _heap) {
+        delete _heap;
+    }
+};
+
+template <typename T> ForwardList<T>::reference ForwardList<T>::front() {
+    assert(!empty());
+    return _front->value;
+}
+
+template <typename T> ForwardList<T>::const_reference ForwardList<T>::front() const {
+    assert(!empty());
+    return _front->value;
+}
+
+template <typename T> void ForwardList<T>::push_front(const T &value) {
+    Node *new_node;
+    if (_spare != nullptr) {
+        new_node = _spare;
+        _spare = _spare->next;
+    } else {
+        new_node = _heap->alloc<Node>();
+    }
+    new_node->next = _front;
+    new_node->value = value;
+    _front = new_node;
+}
+
+template <typename T> void ForwardList<T>::push_front(T &&value) {
+    Node *new_node;
+    if (_spare != nullptr) {
+        new_node = _spare;
+        _spare = _spare->previous;
+    } else {
+        new_node = _heap->alloc<Node>();
+    }
+    new_node->next = _front;
+    new_node->value = std::move(value);
+    _front = new_node;
+}
+
+template <typename T> template <class... Args> void ForwardList<T>::emplace_front(Args &&...args) {
+    Node *new_node;
+    if (_spare != nullptr) {
+        new_node = _spare;
+        _spare = _spare->next;
+    } else {
+        new_node = _heap->alloc<Node>();
+    }
+    new_node->next = _front;
+    new (&new_node->value) T(args...);
+    _front = new_node;
+}
+
+template <typename T> void ForwardList<T>::pop_front() {
+    if (_front == nullptr) {
+        return;
+    }
+
+    Node *next = _front->next;
+    T value = _front->value;
+
+    _front->next = _spare;
+    _spare = _front;
+
+    _front = next;
+}
+
+#ifdef SWIFT_TESTING
+
+class UInt64ForwardList : public ForwardList<uint64_t> {
+  public:
+    static UInt64ForwardList *create();
+    static void destroy(UInt64ForwardList *value);
+
+    bool empty() const noexcept;
+
+    uint64_t front();
+
+    void push_front(const uint64_t &element);
+    void push_front(uint64_t &&element);
+
+    void pop_front();
+
+} SWIFT_UNSAFE_REFERENCE;
+
+UInt64ForwardList *UInt64ForwardList::create() { return new UInt64ForwardList(); }
+
+void UInt64ForwardList::destroy(UInt64ForwardList *value) { delete value; }
+
+bool UInt64ForwardList::empty() const noexcept { return ForwardList<uint64_t>::empty(); }
+
+uint64_t UInt64ForwardList::front() { return ForwardList<uint64_t>::front(); }
+
+void UInt64ForwardList::push_front(const uint64_t &element) { ForwardList<uint64_t>::push_front(element); }
+
+void UInt64ForwardList::push_front(uint64_t &&element) { ForwardList<uint64_t>::push_front(element); }
+
+void UInt64ForwardList::pop_front() { ForwardList<uint64_t>::pop_front(); }
+
+#endif
+
+} /* namespace util */
+
+OG_ASSUME_NONNULL_END
+
+#endif /* OPENGRAPH_CXX_UTIL_FORWARD_LIST_HPP */

Sources/OpenGraphCxx/include/OpenGraphCxx/Util/HashTable.hpp
@@ -1,0 +1,133 @@
+//
+//  HashTable.hpp
+//  OpenGraphCxx
+//
+//  Status: Complete
+//  Modified based Compute code
+
+#ifndef OPENGRAPH_CXX_UTIL_HASHTABLE_HPP
+#define OPENGRAPH_CXX_UTIL_HASHTABLE_HPP
+
+#include <OpenGraph/OGBase.h>
+#include <swift/bridging>
+
+OG_ASSUME_NONNULL_BEGIN
+
+namespace util {
+
+class Heap;
+
+uint64_t string_hash(char const *str);
+
+class UntypedTable {
+public:
+    using key_type = const void *_Nonnull;
+    using nullable_key_type = const void *_Nullable;
+    using value_type = const void *_Nullable;
+    using size_type = uint64_t;
+    using hasher = uint64_t (*)(void const *);
+    using key_equal = bool (*)(void const *, void const *);
+    using key_callback = void (*)(const key_type);
+    using value_callback = void (*)(const value_type);
+    using entry_callback = void (*)(const key_type, const value_type, void *context);
+
+private:
+    struct HashNode {
+        HashNode *next;
+        key_type key;
+        value_type value;
+        uint64_t hash_value;
+    };
+    using Bucket = HashNode *_Nonnull;
+
+    hasher _hash;
+    key_equal _compare;
+    key_callback _did_remove_key;
+    value_callback _did_remove_value;
+    Heap *_heap;
+    HashNode *_spare_node;
+    Bucket *_Nonnull _buckets;
+    uint64_t _count;
+    uint64_t _bucket_mask;
+    uint32_t _bucket_mask_width;
+    bool _is_heap_owner;
+    bool _compare_by_pointer;
+
+    // Managing buckets
+    void create_buckets();
+    void grow_buckets();
+
+public:
+    static UntypedTable *create();
+    static void destroy(UntypedTable *value);
+
+    UntypedTable();
+    UntypedTable(hasher _Nullable custom_hasher, key_equal _Nullable custom_compare,
+                 key_callback _Nullable did_remove_key, value_callback _Nullable did_remove_value,
+                 Heap *_Nullable heap);
+    ~UntypedTable();
+
+    // non-copyable
+    UntypedTable(const UntypedTable &) = delete;
+    UntypedTable &operator=(const UntypedTable &) = delete;
+
+    // non-movable
+    UntypedTable(UntypedTable &&) = delete;
+    UntypedTable &operator=(UntypedTable &&) = delete;
+
+    // Lookup
+    bool empty() const noexcept { return _count == 0; };
+    size_type count() const noexcept { return _count; };
+    value_type lookup(key_type key, nullable_key_type *_Nullable found_key) const noexcept;
+    void for_each(entry_callback body, void *context) const;
+
+    // Modifiers
+    bool insert(const key_type key, const value_type value);
+    bool remove(const key_type key);
+    bool remove_ptr(const key_type key);
+} SWIFT_UNSAFE_REFERENCE;
+
+template <typename Key, typename Value> class Table : public UntypedTable {
+public:
+    using key_type = Key;
+    using value_type = Value;
+    using hasher = uint64_t (*)(const key_type);
+    using key_equal = bool (*)(const key_type, const key_type);
+    using key_callback = void (*)(const key_type);
+    using value_callback = void (*)(const value_type);
+    using entry_callback = void (*)(const key_type, const value_type, void *context);
+
+    Table() : UntypedTable() {};
+    Table(hasher _Nullable custom_hasher, key_equal _Nullable custom_compare, key_callback _Nullable did_remove_key,
+          value_callback _Nullable did_remove_value, Heap *_Nullable heap)
+        : UntypedTable(reinterpret_cast<UntypedTable::hasher>(custom_hasher),
+                       reinterpret_cast<UntypedTable::key_equal>(custom_compare),
+                       reinterpret_cast<UntypedTable::key_callback>(did_remove_key),
+                       reinterpret_cast<UntypedTable::value_callback>(did_remove_value), heap) {};
+
+    // Lookup
+
+    value_type lookup(const key_type key, key_type *_Nullable found_key) const noexcept {
+        auto result = UntypedTable::lookup(*(void **)&key,
+                                           reinterpret_cast<UntypedTable::nullable_key_type *_Nullable>(found_key));
+        return *(value_type *)&result;
+    };
+
+    void for_each(entry_callback _Nonnull body, void *_Nullable context) const {
+        UntypedTable::for_each((UntypedTable::entry_callback)body, context);
+    };
+
+    // Modifying entries
+
+    bool insert(const key_type key, const value_type value) {
+        return UntypedTable::insert(*(void **)&key, *(void **)&value);
+    };
+    bool remove(const key_type key) { return UntypedTable::remove(*(void **)&key); };
+    bool remove_ptr(const key_type key) { return UntypedTable::remove_ptr(key); };
+};
+
+} /* namespace util */
+
+OG_ASSUME_NONNULL_END
+
+#endif /* OPENGRAPH_CXX_UTIL_HASHTABLE_HPP */

OpenSwiftUIProject/OpenAttributeGraph

Sources/OpenAttributeGraphCxx/Util/Heap.cpp
@@ -1,0 +1,99 @@
+//
+//  Heap.cpp
+//  OpenAttributeGraphCxx
+//
+//  Status: Complete
+//  Modified based Compute code
+
+#include <OpenAttributeGraphCxx/Util/Heap.hpp>
+#include <algorithm>
+
+namespace util {
+
+constexpr size_t default_increment = 0x2000;
+
+Heap *Heap::create(char *_Nullable start, size_t capacity, size_t increment) {
+    return new Heap(start, capacity, increment);
+}
+
+void Heap::destroy(Heap *value) { delete value; }
+
+Heap::Heap(char *start, size_t capacity, size_t increment) {
+    // enforce minimum but treat 0 as the default
+    size_t effective_increment = increment > 0 ? std::max(increment, minimum_increment) : default_increment;
+
+    _increment = effective_increment;
+    _node = nullptr;
+    reset(start, capacity);
+};
+
+util::Heap::~Heap() { reset(nullptr, 0); }
+
+void *util::Heap::alloc_(size_t size) {
+    if (_capacity >= size) {
+        char *result = _free_start;
+        _free_start += size;
+        _capacity -= size;
+        return result;
+    }
+
+    if (size <= minimum_increment) {
+        int64_t increment = _increment;
+        char *buffer = static_cast<char *>(malloc(increment));
+
+        _free_start = buffer;
+        _capacity = increment;
+
+        Node *node = alloc<Node>();
+
+        node->next = _node;
+        node->buffer = buffer;
+        _node = node;
+
+        char *result = _free_start;
+        _free_start += size;
+        _capacity -= size;
+        return result;
+    }
+
+    Node *node = alloc<Node>();
+    void *result = malloc(size);
+    if (result) {
+        node->next = _node;
+        node->buffer = result;
+        _node = node;
+    }
+    return result;
+}
+
+void util::Heap::reset(char *_Nullable start, size_t capacity) {
+    while (_node) {
+        void *buffer = _node->buffer;
+        _node = _node->next;
+        free(buffer);
+    }
+
+    constexpr uintptr_t alignment_mask = sizeof(char *) - 1;
+    char *aligned_start = (char *)(((uintptr_t)start + alignment_mask) & ~alignment_mask);
+
+    bool prealigned = ((uintptr_t)start & alignment_mask) == 0;
+    _free_start = prealigned ? start : aligned_start;
+    _capacity = capacity + (start - aligned_start);
+}
+
+size_t util::Heap::num_nodes() const {
+    size_t count = 0;
+    for (Node *node = _node; node != nullptr; node = node->next) {
+        count += 1;
+    }
+    return count;
+}
+
+void util::Heap::print() const {
+    fprintf(stdout, "Nodes\n");
+    for (Node *node = _node; node != nullptr; node = node->next) {
+        fprintf(stdout, "address=%-16p; buffer=%-16p; next=%-16p\n", node, node->buffer, node->next);
+    }
+}
+
+}; /* namespace util */

Sources/OpenAttributeGraphCxx/include/OpenAttributeGraphCxx/Util/ForwardList.hpp
@@ -1,0 +1,179 @@
+//
+//  ForwardList.hpp
+//  OpenAttributeGraphCxx
+//
+//  Status: Complete
+//  Modified based Compute code
+
+#ifndef OPENATTRIBUTEGRAPH_CXX_UTIL_FORWARD_LIST_HPP
+#define OPENATTRIBUTEGRAPH_CXX_UTIL_FORWARD_LIST_HPP
+
+#include <OpenAttributeGraph/OAGBase.h>
+#include <OpenAttributeGraphCxx/Util/Heap.hpp>
+
+OAG_ASSUME_NONNULL_BEGIN
+
+namespace util {
+
+/// ForwardList is a linked list container that uses util::Heap to allocate nodes,
+/// reusing previously removed nodes where possible.
+template <typename T> class ForwardList {
+  public:
+    using reference = T &;
+    using const_reference = const T &;
+
+  private:
+    struct Node {
+        Node *_Nullable next;
+        T value;
+    };
+
+    util::Heap *_heap;
+    Node *_Nullable _front;
+    Node *_Nullable _spare;
+    bool _is_heap_owner;
+
+  public:
+    ForwardList();
+    ForwardList(util::Heap *heap);
+    ~ForwardList();
+
+    // non-copyable
+    ForwardList(const ForwardList &) = delete;
+    ForwardList &operator=(const ForwardList &) = delete;
+
+    // non-movable
+    ForwardList(ForwardList &&) = delete;
+    ForwardList &operator=(ForwardList &&) = delete;
+
+    // MARK: Element access
+
+    reference front();
+    const_reference front() const;
+
+    // MARK: Capacity
+
+    bool empty() const noexcept { return _front == nullptr; }
+
+    // MARK: Modifiers
+
+    void push_front(const T &value);
+    void push_front(T &&value);
+    template <class... Args> void emplace_front(Args &&...args);
+
+    void pop_front();
+};
+
+template <typename T>
+ForwardList<T>::ForwardList() : _heap(new Heap(nullptr, 0, util::Heap::minimum_increment)), _is_heap_owner(true){};
+
+template <typename T> ForwardList<T>::ForwardList(util::Heap *heap) : _heap(heap), _is_heap_owner(false){};
+
+template <typename T> ForwardList<T>::~ForwardList() {
+    if (_is_heap_owner && _heap) {
+        delete _heap;
+    }
+};
+
+template <typename T> ForwardList<T>::reference ForwardList<T>::front() {
+    assert(!empty());
+    return _front->value;
+}
+
+template <typename T> ForwardList<T>::const_reference ForwardList<T>::front() const {
+    assert(!empty());
+    return _front->value;
+}
+
+template <typename T> void ForwardList<T>::push_front(const T &value) {
+    Node *new_node;
+    if (_spare != nullptr) {
+        new_node = _spare;
+        _spare = _spare->next;
+    } else {
+        new_node = _heap->alloc<Node>();
+    }
+    new_node->next = _front;
+    new_node->value = value;
+    _front = new_node;
+}
+
+template <typename T> void ForwardList<T>::push_front(T &&value) {
+    Node *new_node;
+    if (_spare != nullptr) {
+        new_node = _spare;
+        _spare = _spare->previous;
+    } else {
+        new_node = _heap->alloc<Node>();
+    }
+    new_node->next = _front;
+    new_node->value = std::move(value);
+    _front = new_node;
+}
+
+template <typename T> template <class... Args> void ForwardList<T>::emplace_front(Args &&...args) {
+    Node *new_node;
+    if (_spare != nullptr) {
+        new_node = _spare;
+        _spare = _spare->next;
+    } else {
+        new_node = _heap->alloc<Node>();
+    }
+    new_node->next = _front;
+    new (&new_node->value) T(args...);
+    _front = new_node;
+}
+
+template <typename T> void ForwardList<T>::pop_front() {
+    if (_front == nullptr) {
+        return;
+    }
+
+    Node *next = _front->next;
+    T value = _front->value;
+
+    _front->next = _spare;
+    _spare = _front;
+
+    _front = next;
+}
+
+#ifdef SWIFT_TESTING
+
+class UInt64ForwardList : public ForwardList<uint64_t> {
+  public:
+    static UInt64ForwardList *create();
+    static void destroy(UInt64ForwardList *value);
+
+    bool empty() const noexcept;
+
+    uint64_t front();
+
+    void push_front(const uint64_t &element);
+    void push_front(uint64_t &&element);
+
+    void pop_front();
+
+} SWIFT_UNSAFE_REFERENCE;
+
+UInt64ForwardList *UInt64ForwardList::create() { return new UInt64ForwardList(); }
+
+void UInt64ForwardList::destroy(UInt64ForwardList *value) { delete value; }
+
+bool UInt64ForwardList::empty() const noexcept { return ForwardList<uint64_t>::empty(); }
+
+uint64_t UInt64ForwardList::front() { return ForwardList<uint64_t>::front(); }
+
+void UInt64ForwardList::push_front(const uint64_t &element) { ForwardList<uint64_t>::push_front(element); }
+
+void UInt64ForwardList::push_front(uint64_t &&element) { ForwardList<uint64_t>::push_front(element); }
+
+void UInt64ForwardList::pop_front() { ForwardList<uint64_t>::pop_front(); }
+
+#endif
+
+} /* namespace util */
+
+OAG_ASSUME_NONNULL_END
+
+#endif /* OPENATTRIBUTEGRAPH_CXX_UTIL_FORWARD_LIST_HPP */

Sources/OpenAttributeGraphCxx/include/OpenAttributeGraphCxx/Util/HashTable.hpp
@@ -1,0 +1,133 @@
+//
+//  HashTable.hpp
+//  OpenAttributeGraphCxx
+//
+//  Status: Complete
+//  Modified based Compute code
+
+#ifndef OPENATTRIBUTEGRAPH_CXX_UTIL_HASHTABLE_HPP
+#define OPENATTRIBUTEGRAPH_CXX_UTIL_HASHTABLE_HPP
+
+#include <OpenAttributeGraph/OAGBase.h>
+#include <swift/bridging>
+
+OAG_ASSUME_NONNULL_BEGIN
+
+namespace util {
+
+class Heap;
+
+uint64_t string_hash(char const *str);
+
+class UntypedTable {
+public:
+    using key_type = const void *_Nonnull;
+    using nullable_key_type = const void *_Nullable;
+    using value_type = const void *_Nullable;
+    using size_type = uint64_t;
+    using hasher = uint64_t (*)(void const *);
+    using key_equal = bool (*)(void const *, void const *);
+    using key_callback = void (*)(const key_type);
+    using value_callback = void (*)(const value_type);
+    using entry_callback = void (*)(const key_type, const value_type, void *context);
+
+private:
+    struct HashNode {
+        HashNode *next;
+        key_type key;
+        value_type value;
+        uint64_t hash_value;
+    };
+    using Bucket = HashNode *_Nonnull;
+
+    hasher _hash;
+    key_equal _compare;
+    key_callback _did_remove_key;
+    value_callback _did_remove_value;
+    Heap *_heap;
+    HashNode *_spare_node;
+    Bucket *_Nonnull _buckets;
+    uint64_t _count;
+    uint64_t _bucket_mask;
+    uint32_t _bucket_mask_width;
+    bool _is_heap_owner;
+    bool _compare_by_pointer;
+
+    // Managing buckets
+    void create_buckets();
+    void grow_buckets();
+
+public:
+    static UntypedTable *create();
+    static void destroy(UntypedTable *value);
+
+    UntypedTable();
+    UntypedTable(hasher _Nullable custom_hasher, key_equal _Nullable custom_compare,
+                 key_callback _Nullable did_remove_key, value_callback _Nullable did_remove_value,
+                 Heap *_Nullable heap);
+    ~UntypedTable();
+
+    // non-copyable
+    UntypedTable(const UntypedTable &) = delete;
+    UntypedTable &operator=(const UntypedTable &) = delete;
+
+    // non-movable
+    UntypedTable(UntypedTable &&) = delete;
+    UntypedTable &operator=(UntypedTable &&) = delete;
+
+    // Lookup
+    bool empty() const noexcept { return _count == 0; };
+    size_type count() const noexcept { return _count; };
+    value_type lookup(key_type key, nullable_key_type *_Nullable found_key) const noexcept;
+    void for_each(entry_callback body, void *context) const;
+
+    // Modifiers
+    bool insert(const key_type key, const value_type value);
+    bool remove(const key_type key);
+    bool remove_ptr(const key_type key);
+} SWIFT_UNSAFE_REFERENCE;
+
+template <typename Key, typename Value> class Table : public UntypedTable {
+public:
+    using key_type = Key;
+    using value_type = Value;
+    using hasher = uint64_t (*)(const key_type);
+    using key_equal = bool (*)(const key_type, const key_type);
+    using key_callback = void (*)(const key_type);
+    using value_callback = void (*)(const value_type);
+    using entry_callback = void (*)(const key_type, const value_type, void *context);
+
+    Table() : UntypedTable() {};
+    Table(hasher _Nullable custom_hasher, key_equal _Nullable custom_compare, key_callback _Nullable did_remove_key,
+          value_callback _Nullable did_remove_value, Heap *_Nullable heap)
+        : UntypedTable(reinterpret_cast<UntypedTable::hasher>(custom_hasher),
+                       reinterpret_cast<UntypedTable::key_equal>(custom_compare),
+                       reinterpret_cast<UntypedTable::key_callback>(did_remove_key),
+                       reinterpret_cast<UntypedTable::value_callback>(did_remove_value), heap) {};
+
+    // Lookup
+
+    value_type lookup(const key_type key, key_type *_Nullable found_key) const noexcept {
+        auto result = UntypedTable::lookup(*(void **)&key,
+                                           reinterpret_cast<UntypedTable::nullable_key_type *_Nullable>(found_key));
+        return *(value_type *)&result;
+    };
+
+    void for_each(entry_callback _Nonnull body, void *_Nullable context) const {
+        UntypedTable::for_each((UntypedTable::entry_callback)body, context);
+    };
+
+    // Modifying entries
+
+    bool insert(const key_type key, const value_type value) {
+        return UntypedTable::insert(*(void **)&key, *(void **)&value);
+    };
+    bool remove(const key_type key) { return UntypedTable::remove(*(void **)&key); };
+    bool remove_ptr(const key_type key) { return UntypedTable::remove_ptr(key); };
+};
+
+} /* namespace util */
+
+OAG_ASSUME_NONNULL_END
+
+#endif /* OPENATTRIBUTEGRAPH_CXX_UTIL_HASHTABLE_HPP */
97921057bae529708e50e6fa65a8248fe8c96388
Kyle <kyle201817146@gmail.com>
2025-09-06 16:38:49 +0800

    Improve metadata debug functionality with better field handling (#182)

--- Tests/OpenAttributeGraphShimsTests/MetadataDebugTests.swift
+++ Tests/OpenAttributeGraphShimsTests/MetadataDebugTests.swift
@@ -5,6 +5,14 @@
 @_spi(Debug) import OpenAttributeGraphShims
 import Testing
 
+#if canImport(UIKit)
+import UIKit
+private typealias PlatformView = UIView
+#elseif canImport(AppKit)
+import AppKit
+private typealias PlatformView = NSView
+#endif
+
 @Suite(.disabled(if: !attributeGraphEnabled, "forEachField is not implemented for OAG"))
 struct MetadataDebugTests {
     struct Demo1 {
@@ -16,7 +24,7 @@ struct MetadataDebugTests {
         var a: Int = .zero
         var b: Double = .zero
     }
-    
+
     @Test
     func layout() {
         #expect(Metadata(Demo1.self).layoutDescription == #"""
@@ -35,5 +43,25 @@ struct MetadataDebugTests {
         
         """#)
     }
-    
+
+    #if canImport(UIKit) || canImport(AppKit)
+    @Test
+    func unknownFields() {
+        class DemoView: PlatformView {
+            var a: Int = .zero
+            var b: Double = .zero
+        }
+        let layoutDescription = Metadata(DemoView.self).layoutDescription.split(separator: "\n").map(String.init)
+        #expect(layoutDescription[0] == "class DemoView {")
+        #expect(layoutDescription[1].contains("var a: Int // offset = "))
+        #expect(layoutDescription[2].contains("var b: Double // offset = "))
+        #expect(layoutDescription[3] == "}")
+    }
+    #endif
+}
+
+extension Int {
+    fileprivate var hex: String {
+        "0x\(String(format:"%X", self))"
+    }
 }

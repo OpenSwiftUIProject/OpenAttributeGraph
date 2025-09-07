e3898e61195fa49f569a5a59c050fb8cbbb311f6
Kyle <kyle201817146@gmail.com>
2025-09-08 02:09:22 +0800

    Fix OpenAttributeGraphVersionNumber warning

--- Sources/OpenAttributeGraphCxx/Version/OAGVersion.c
+++ Sources/OpenAttributeGraphCxx/Version/OAGVersion.c
@@ -6,11 +6,11 @@
 #include <OpenAttributeGraph/OAGVersion.h>
 
 #if OPENATTRIBUTEGRAPH_RELEASE == OPENATTRIBUTEGRAPH_RELEASE_2024
-const double OpenAttributeGraphVersionNumber __attribute__ ((used)) = (double)6.0;
+double OpenAttributeGraphVersionNumber __attribute__ ((used)) = (double)6.0;
 const unsigned char OpenAttributeGraphVersionString[] __attribute__ ((used)) = "@(#)PROAGRAM:OpenAttributeGraph  PROJECT:OpenAttributeGraph-6.0.87\n";
 const uint64_t OAGVersion = 0x2001e;
 #elif OPENATTRIBUTEGRAPH_RELEASE == OPENATTRIBUTEGRAPH_RELEASE_2021
-const double OpenAttributeGraphVersionNumber __attribute__ ((used)) = (double)3.2;
+double OpenAttributeGraphVersionNumber __attribute__ ((used)) = (double)3.2;
 const unsigned char OpenAttributeGraphVersionString[] __attribute__ ((used)) = "@(#)PROAGRAM:OpenAttributeGraph  PROJECT:OpenAttributeGraph-3.2.1\n";
 const uint64_t OAGVersion = 0x20014;
 #endif

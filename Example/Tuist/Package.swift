eb494fb9ce31e78e7e0220e7b30793d809e13450
Kyle <kyle201817146@gmail.com>
2026-07-27 03:27:38 +0800

    Enable Tuist caching and build insights in CI (#230)

--- Example/Tuist/Package.swift
+++ Example/Tuist/Package.swift
@@ -17,8 +17,5 @@ let packageSettings = PackageSettings(
 
 let package = Package(
     name: "ExampleDependencies",
-    dependencies: [
-        .package(path: "../../"),
-        .package(url: "https://github.com/apple/swift-numerics", from: "1.1.1"),
-    ]
+    dependencies: []
 )
--- Tuist.swift
+++ Tuist.swift
@@ -1,8 +1,13 @@
 import ProjectDescription
 
 let tuist = Tuist(
+    fullHandle: "OpenSwiftUIProject/openattributegraph",
+    xcodeCache: .xcodeCache(
+        upload: Environment.isCI
+    ),
     project: .tuist(
         generationOptions: .options(
+            enableCaching: true,
             manifestEnvironment: [
                 "OPENSWIFTUI_*",
                 "OPENATTRIBUTEGRAPH_*",

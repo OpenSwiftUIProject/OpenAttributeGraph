1d8e762338d6447c0b8c98cd08ccf1efca981e82
Kyle <kyle201817146@gmail.com>
2026-09-08 01:44:31 +0800

    chore: restore persistent local compilation caching

--- Example/Project.swift
+++ Example/Project.swift
@@ -2,6 +2,16 @@ import ProjectDescription
 
 let project = Project(
     name: "Example",
+    settings: .settings(
+        base: [
+            // Keep local compilation artifacts outside temporary CI build directories.
+            "COMPILATION_CACHE_ENABLE_CACHING": "YES",
+            "COMPILATION_CACHE_CAS_PATH": "$(HOME)/Library/Developer/Xcode/DerivedData/CompilationCache.noindex",
+            "COMPILATION_CACHE_KEEP_CAS_DIRECTORY": "YES",
+            "COMPILATION_CACHE_REMOTE_SERVICE_PATH": "",
+            "COMPILATION_CACHE_ENABLE_PLUGIN": "NO",
+        ]
+    ),
     targets: [
         .target(
             name: "Example",
--- Example/Tuist/Package.swift
+++ Example/Tuist/Package.swift
@@ -5,7 +5,18 @@ import PackageDescription
 #if TUIST
 import ProjectDescription
 
-let packageSettings = PackageSettings()
+let packageSettings = PackageSettings(
+    baseSettings: .settings(
+        base: [
+            // Keep local compilation artifacts outside temporary CI build directories.
+            "COMPILATION_CACHE_ENABLE_CACHING": "YES",
+            "COMPILATION_CACHE_CAS_PATH": "$(HOME)/Library/Developer/Xcode/DerivedData/CompilationCache.noindex",
+            "COMPILATION_CACHE_KEEP_CAS_DIRECTORY": "YES",
+            "COMPILATION_CACHE_REMOTE_SERVICE_PATH": "",
+            "COMPILATION_CACHE_ENABLE_PLUGIN": "NO",
+        ]
+    )
+)
 #endif
 
 let package = Package(

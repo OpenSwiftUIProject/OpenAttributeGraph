ccacd7d37b2edd7fe6d84fd8dee74b901df80335
Kyle <kyle201817146@gmail.com>
2026-09-07 22:20:13 +0800

    chore: disable remote Tuist caching (#239)

--- Example/Tuist.swift
+++ Example/Tuist.swift
@@ -1,18 +1,20 @@
 import ProjectDescription
 
+// Keep remote caches disabled to reduce metered network traffic.
 let tuist = Tuist(
     fullHandle: "OpenSwiftUIProject/openattributegraph",
     xcodeCache: .xcodeCache(
-        upload: Environment.isCI
+        upload: false
     ),
     project: .tuist(
         generationOptions: .options(
             optionalAuthentication: true,
-            enableCaching: Environment.isCI,
+            enableCaching: false,
             manifestEnvironment: [
                 "OPENSWIFTUI_*",
                 "OPENATTRIBUTEGRAPH_*",
             ]
-        )
+        ),
+        cacheOptions: .options(storages: [.local])
     )
 )
